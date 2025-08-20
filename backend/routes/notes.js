e3ac2bec6c637eefe797d2cebdb07252e35a6f96
DilverSingh23 <dsinghh23@gmail.com>
2025-08-20 11:23:20 -0400

    create update and delete note api endpoints

--- backend/routes/notes.js
+++ backend/routes/notes.js
@@ -5,7 +5,7 @@ import express from "express"
 const router = express.Router()
 let notesCollection = db.collection("notes")
 
-
+// Get all user notes
 router.get("/user/:userId", async (req, res) => {
     try {
         const id = parseInt(req.params.userId, 10)
@@ -22,10 +22,12 @@ router.get("/user/:userId", async (req, res) => {
     }
 })
 
+// Read a specific user note
 router.get("/note/:id", async (req, res) => {
     try {
+        const userId = parseInt(req.params.userId, 10)
         const noteId = ObjectId.createFromHexString(req.params.id)
-        const userNote = await notesCollection.findOne({ _id: noteId })
+        const userNote = await notesCollection.findOne({ _id: noteId, userId: userId})
         if (userNote) {
             res.status(200).json(userNote)
         }
@@ -38,6 +40,7 @@ router.get("/note/:id", async (req, res) => {
     }
 })
 
+// Create a new note
 router.post("/createNote", async(req, res) => {
     try {
         const userNotes = await notesCollection
@@ -59,6 +62,44 @@ router.post("/createNote", async(req, res) => {
     } catch(err) {
         console.error("Server error trying to make new note: ", err)
         res.status(500).json({ error: "Unable to create new note." })
+    }
+})
+
+// Edit an existing note
+router.patch("/updateNote/:id", async (req, res) => {
+    try {
+        const noteId = { _id: ObjectId.createFromHexString(req.params.id) }
+        const newTitle = req.body.title
+        const newMessage = req.body.message
+        if (newTitle.length == 0 || newMessage.length == 0) {
+            res.status(404).json({ error: "Your title or message can not be empty!" })
+        }
+        const updates = {
+            $set: {
+                title: req.body.title,
+                message: req.body.message,
+                lastUpdated: new Date()
+            }
+        }
+        let result = await notesCollection.updateOne(noteId, updates)
+        console.log("Succesfully updated note")
+        res.status(200).json(result)
+    } catch(err) {
+        console.error("Node update error: ", err)
+        res.status(500).json({ error: "Unable to edit note." })
+    }
+})
+
+// Delete an existing note
+router.delete("/deleteNote/:id", async (req, res) => {
+    try {
+        const noteId = { _id: ObjectId.createFromHexString(req.params.id) }
+        let result = await notesCollection.deleteOne(noteId)
+        console.log("Successfully deleted note")
+        res.status(200).json(result)
+    } catch(err) {
+        console.error("Node deletion error: ", err)
+        res.status(404).json({ error: "Unable to delete note." })
     }
 })
 
