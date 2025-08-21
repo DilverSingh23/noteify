02ab84fcdd620ffab7fb5454a1cfa09bd46f7e6d
DilverSingh23 <dsinghh23@gmail.com>
2025-08-20 21:30:44 -0400

    implement page routing with react router

---
+++ frontend/src/Login.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Login = () => {
+    return (
+        <section className="w-screen min-h-screen flex justify-center items-center">
+            Hello
+        </section>
+    )
+}
+
+export default Login
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,14 +1,19 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 const Navbar = () => {
     return (
         <div className="w-screen h-30 flex items-center justify-center gap-150">
-            <div className="flex w-fit items-center justify-center gap-4 hover:cursor-pointer">
-                <img className="h-15 w-15"src="/noteify-logo.png" />
-                <h1 className="font-inter text-black text-4xl font-extrabold">noteify</h1>
-            </div>
+            <Link to={"/"}>
+                <div className="flex w-fit items-center justify-center gap-4 hover:cursor-pointer">
+                    <img className="h-15 w-15"src="/noteify-logo.png" />
+                    <h1 className="font-inter text-black text-4xl font-extrabold">noteify</h1>
+                </div>
+            </Link>
             <div className="flex w-fit items-center justify-center gap-3">
-                <button className="bg-[#787CFF] font-inter text-white font-bold p-3 pl-5 pr-4 rounded-3xl hover:text-black hover:bg-pink-200 hover:cursor-pointer">Log In</button>
+                <Link to={"/login"}>
+                    <button className="bg-[#787CFF] font-inter text-white font-bold p-3 pl-5 pr-4 rounded-3xl hover:text-black hover:bg-pink-200 hover:cursor-pointer">Log In</button>
+                </Link>
                 <button className="bg-[#787CFF] font-inter text-white font-bold p-3 pl-5 pr-4 rounded-3xl hover:text-black hover:bg-pink-200 hover:cursor-pointer">Sign Up</button>
             </div>
         </div>
