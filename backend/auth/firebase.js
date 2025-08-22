db917406ded9fa375ad19f00d4ff4b389f6aac1c
DilverSingh23 <dsinghh23@gmail.com>
2025-08-22 02:30:46 -0400

    reroute firebase service account info

--- backend/auth/firebase.js
+++ backend/auth/firebase.js
@@ -1,5 +1,11 @@
 import admin from "firebase-admin"
-import serviceAccount from "../firebaseService.json" with { type: 'json' }
+
+const serviceAccount = {
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL
+}
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
